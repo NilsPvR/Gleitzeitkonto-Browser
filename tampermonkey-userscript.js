da94c1fc02b4782a1ed1771642aa926ba467d137
NilsPvR <nilsundgrk@gmx.de>
2022-09-15 09:50:47 +0200

    imporve server contact - async; support internSite overhauled code structure

--- tampermonkey-userscript.js
+++ tampermonkey-userscript.js
@@ -6,54 +6,97 @@
 // @description  Überstunden einfügen in Fiori
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @match        *://*.ondemand.com/*
+// @match        https://bgp.btcsap.btc-ag.com:44300/*
 
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
-    let fetchedData;
-    fetch('http://127.0.0.1:3000')
-        .then((response) => response.json())
-        .then((data) => fetchedData = data)
-        .catch((error) => console.error(error));
+    /* ==========================================================================================
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Config <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+    const config = {};
 
-    const addFloatingDisplay = () => {
+    // Check if extern or intern Fiori website, since these have different amounts of icons    
+    if (window.location.origin == 'https://bgp.btcsap.btc-ag.com:44300') { // Intern Fiori
+        config.amountIcons = 4; // amount of icons to wait for to load
+        config.sideDistance = '11rem'; // css margin from the right for floating display
+    }    
+    else { // extern
+        config.amountIcons = 3;
+        config.sideDistance = '9rem';
+    }
+
+
+    /* ==========================================================================================
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Functions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
+    const fetchServer = async () => {
+        try {
+            let url = 'http://127.0.0.1:3000';
+            let data = await (await fetch(url)).json();
+            return data
+        }
+        catch (e) {
+            console.log(e.message);
+            console.log(e.message == 'NetworkError when attempting to fetch resource.')
+            if (e.message == 'NetworkError when attempting to fetch resource.') {
+                console.log('whoops\n' + e);
+                return {errorMessage: 'Der Lokale-Server wurde nicht gestartet!'}
+                
+            }
+            console.error(e);
+        }
+        
+    };
+    const getDisplayText = async () => {
+        const res = await fetchServer();
+        if (res.errorMessage) return `Fehler: ${res.errorMessage}`;
+        else if (!res || !res.konto) return 'Fehler: Keine Daten vom Lokalen-Server geladen';
+        else return `Gleitzeitkonto: ${res.konto}`;
+    };
+    
+
+    const addFloatingDisplay = (pDisplayText) => {
         const canvas = document.getElementById('canvas'); // main page element is the (almost) only one loaded when DOM is loaded
         canvas.insertAdjacentHTML('beforebegin',
-            `<h3 id="gleitzeitkonto-canvas-headline"style="float: right; margin-top: 11px; margin-right: 9rem; color: rgb(1, 56, 105);">Gleitzeitkonto: ${fetchedData?.gleitzeitkonto ? fetchedData.gleitzeitkonto : 'error'}</h3>`);
+            `<h3 id="gleitzeitkonto-canvas-headline"style="float: right; margin-top: 11px; margin-right: ${config.sideDistance}; color: rgb(1, 56, 105);">${pDisplayText ?? 'unknown error'}</h3>`);
     };
 
-    const addInsertedDisplay = (pHeaderBar) => {
+    const addInsertedDisplay = (pHeaderBar, pDisplayText) => {
         const oldDisplay = document.getElementById('gleitzeitkonto-canvas-headline');
         if (oldDisplay) oldDisplay.remove(); // delete the old display
 
-        pHeaderBar.innerHTML += `<h3 style="display:flex; align-self: center; color: rgb(1, 56, 105);">Gleitzeitkonto: ${fetchedData?.gleitzeitkonto ? fetchedData.gleitzeitkonto : 'error'}</h3>`; // add new display
+        pHeaderBar.innerHTML += `<h3 style="display:flex; align-self: center; color: rgb(1, 56, 105);">${pDisplayText ?? 'unknown error'}</h3>`; // add new display
     };
 
+    /* ==========================================================================================
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Main Events <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+    const promiseDisplayText = getDisplayText(); // preload display to save time
 
-    if (document.readyState === 'interactive') {
-        console.log('already ready');
-        addFloatingDisplay();
+    if (document.readyState === 'interactive' || document.readyState === 'complete') {
+        addFloatingDisplay('probably loading');
     }
     else {
         // Load event fires too early so no point using that
         window.addEventListener('DOMContentLoaded', (event) => {
-            addFloatingDisplay();
+            addFloatingDisplay('probably loading');
         })
     }
 
     let headerBar;
     let loops = 0; // track how often findHeaderBar ran
+    
+    
 
     // disgusting loop to check once the page has actually loaded
-    const findHeaderBar = setInterval(() => {
+    const findHeaderBar = setInterval(async () => {
         loops++;
         headerBar = document.getElementById('shell-header-hdr-search-container'); // top bar, empty part
         const icons = document.getElementsByClassName('sapUshellShellHeadItmCntnt');
 
-        if (headerBar && icons.length == 3) {
+        if (headerBar && icons.length == config.amountIcons) {
             clearInterval(findHeaderBar);
-            addInsertedDisplay(headerBar);
+            addInsertedDisplay(headerBar, await promiseDisplayText); // make sure DisplayText loaded
         }
         else if (loops > 10) clearInterval(findHeaderBar); // page loaded too long or html got changed
     }, 2000); // will be limited to min. 1000 when tab not focused
