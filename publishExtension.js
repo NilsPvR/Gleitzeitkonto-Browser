65e773a0272f7d4b2c82ea104691573a6fd4f8a8
NilsPvR <nilsundgrk@gmx.de>
2022-12-06 10:46:14 +0100

    fix publishing script, renamed script file

--- publishExtension.js
+++ publishExtension.js
@@ -1,11 +1,12 @@
 const { copy, remove } = require('fs-extra');
 
-
-try {
-    await copy('/extension', '/compressed/extension'); // copies directory with subdirectories
-    await remove('/compressed/extension/script.js'); // remove file
-    console.log('Copied folder succesfully!');
-}
-catch (e) {
-    console.error(e);
-}
+(async function() {
+    try {
+        await copy('./extension', './compressed/extension'); // copies directory with subdirectories
+        await remove('./compressed/extension/script.js'); // remove file
+        console.log('Copied folder succesfully!');
+    }
+    catch (e) {
+        console.error(e);
+    }
+})();
