6b43b200480885d2d53e221276802b0fe9feab00
NilsPvR <nilsundgrk@gmx.de>
2023-03-01 17:27:06 +0100

    update publishing of Extension

--- publishExtension.js
+++ publishExtension.js
@@ -1,12 +1,14 @@
-const { copy, remove } = require('fs-extra');
+const { copy } = require('fs-extra');
+const fs = require('fs');
 const { extname } = require('path');
 
 (async function() {
     try {
         const filterFunc = (src, dest) => {
-            return extname(src) != '.js'; // exclude js files
+            return extname(src) != '.js' && extname(src) != '.scss'; // exclude js and scss files
         };
         await copy('./extension', './compressed/extension', { filter: filterFunc }); // copies directory with subdirectories
+        if (!fs.existsSync('./compressed/Gleitzeitkonto-Browser')) fs.mkdirSync('./compressed/Gleitzeitkonto-Browser'); // create folder
 
         console.log('Copied folder succesfully!');
     }
