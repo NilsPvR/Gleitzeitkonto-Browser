93f69614f645a35c65cdde2bf31ab1fe6f21cd9b
NilsPvR <nilsundgrk@gmx.de>
2023-03-09 17:30:06 +0100

    even more typo fixes

--- publishExtension.js
+++ publishExtension.js
@@ -7,7 +7,7 @@ const { extname } = require('path');
         const filterFunc = (src, dest) => {
             return extname(src) != '.js' && extname(src) != '.scss'; // exclude js and scss files
         };
-        await copy('./extension', './compressed/extension', { filter: filterFunc }); // copies directory with subdirectories
+        await copy('./Extension', './compressed/extension', { filter: filterFunc }); // copies directory with subdirectories
         fs.rmSync('./compressed/extension/url.json', { force: true }); // remove url file
         if (!fs.existsSync('./compressed/Gleitzeitkonto-Browser')) fs.mkdirSync('./compressed/Gleitzeitkonto-Browser'); // create folder
         
