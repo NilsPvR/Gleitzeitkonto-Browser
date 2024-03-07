fad8aad03e3f87fb6296ff427896f0cc8b34133d
NilsPvR <nilsundgrk@gmx.de>
2024-03-07 16:26:29 +0100

    feat: update publishWebserver script to the new file structure

--- publishWebserver.js
+++ publishWebserver.js
@@ -2,30 +2,24 @@ const fs = require('fs');
 
 (async function() {
     try {
-        if (!fs.existsSync('./compressed/unpackedWebserver')) fs.mkdirSync('./compressed/unpackedWebserver'); // create folder
-        if (!fs.existsSync('./compressed/unpackedWebserver/gleitzeitkonto-api')) fs.mkdirSync('./compressed/unpackedWebserver/gleitzeitkonto-api');
+        if (!fs.existsSync('./compressed/companionApp')) fs.mkdirSync('./compressed/companionApp'); // create folder
+        if (!fs.existsSync('./compressed/companionApp/gleitzeitkonto-api')) fs.mkdirSync('./compressed/companionApp/gleitzeitkonto-api');
 
-        if (!fs.existsSync('./compressed/packedWebserver')) fs.mkdirSync('./compressed/packedWebserver');
         if (!fs.existsSync('./compressed/Gleitzeitkonto-Browser-ZIP')) fs.mkdirSync('./compressed/Gleitzeitkonto-Browser-ZIP'); // create folder
 
-        fs.copyFileSync('./Webserver/start-Gleitzeitkonto-Webserver-p.vbs', './compressed/packedWebserver/start-Gleitzeitkonto-Webserver.vbs');
-        fs.copyFileSync('./Webserver/start-Gleitzeitkonto-Webserver-up.vbs', './compressed/unpackedWebserver/start-Gleitzeitkonto-Webserver.vbs');
-        fs.copyFileSync('./Webserver/version.txt', './compressed/unpackedWebserver/version.txt');
-        fs.copyFileSync('./Webserver/version.txt', './compressed/packedWebserver/version.txt');
-        fs.copyFileSync('./Webserver/icon.ico', './compressed/unpackedWebserver/icon.ico');
-        fs.copyFileSync('./Webserver/icon.ico', './compressed/packedWebserver/icon.ico');
-        fs.copyFileSync('./Webserver/main.js', './compressed/unpackedWebserver/main.js');
-        fs.copyFileSync('./Webserver/gleitzeitkonto-api/gleitzeitkonto-api.js', './compressed/unpackedWebserver/gleitzeitkonto-api/gleitzeitkonto-api.js');
+        fs.copyFileSync('./CompanionApp/start-Gleitzeitkonto-Browser CompanionApp.vbs', './compressed/companionApp/start-Gleitzeitkonto-Browser CompanionApp.vbs');
+        fs.copyFileSync('./CompanionApp/version.txt', './compressed/companionApp/version.txt');
+        fs.copyFileSync('./CompanionApp/url.txt', './compressed/companionApp/url.txt');
+        fs.copyFileSync('./CompanionApp/main.js', './compressed/companionApp/main.js');
+        fs.copyFileSync('./CompanionApp/gleitzeitkonto-api/gleitzeitkonto-api.js', './compressed/companionApp/gleitzeitkonto-api/gleitzeitkonto-api.js');
         // only copy the dependencies into the new package to avoid duplicate npm scripts for developers
-        fs.writeFileSync('./compressed/unpackedWebserver/package.json', 
+        fs.writeFileSync('./compressed/companionApp/package.json', 
             '{ "dependencies": ' +
                 JSON.stringify(JSON.parse(fs.readFileSync('./package.json')).dependencies) +
             '}'        
         );
 
-        fs.writeFileSync('./compressed/unpackedWebserver/url.json', '""');
-
-        console.log('Created folder succesfully!');
+        console.log('Created folder and files for CompanionApp succesfully!');
     }
     catch (e) {
         console.error(e);
