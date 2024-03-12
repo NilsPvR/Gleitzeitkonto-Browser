abd2d78dba7bc237cf4b08a18565d07766cf9801
NilsPvR <nilsundgrk@gmx.de>
2024-03-12 18:47:36 +0100

    build: improve build time and streamline packages by using esbuild + do not copy url, api and protocol file

--- publishCompanionApp.js
+++ publishCompanionApp.js
@@ -3,17 +3,13 @@ const fs = require('fs');
 (async function() {
     try {
         if (!fs.existsSync('./compressed/companionApp')) fs.mkdirSync('./compressed/companionApp'); // create folder
-        if (!fs.existsSync('./compressed/companionApp/gleitzeitkonto-api')) fs.mkdirSync('./compressed/companionApp/gleitzeitkonto-api');
 
         if (!fs.existsSync('./compressed/Gleitzeitkonto-Browser-ZIP')) fs.mkdirSync('./compressed/Gleitzeitkonto-Browser-ZIP'); // create folder
 
         fs.copyFileSync('./CompanionApp/version.txt', './compressed/companionApp/version.txt');
-        fs.copyFileSync('./CompanionApp/url.json', './compressed/companionApp/url.json');
         fs.copyFileSync('./CompanionApp/main.js', './compressed/companionApp/main.js');
         fs.copyFileSync('./CompanionApp/manifest.json', './compressed/companionApp/manifest.json');
         fs.copyFileSync('./CompanionApp/manifest-chromium.json', './compressed/companionApp/manifest-chromium.json');
-        fs.copyFileSync('./CompanionApp/protocol.js', './compressed/companionApp/protocol.js');
-        fs.copyFileSync('./CompanionApp/gleitzeitkonto-api/gleitzeitkonto-api.js', './compressed/companionApp/gleitzeitkonto-api/gleitzeitkonto-api.js');
         // only copy the dependencies into the new package to avoid duplicate npm scripts for developers
         fs.writeFileSync('./compressed/companionApp/package.json', 
             '{ "dependencies": ' +
