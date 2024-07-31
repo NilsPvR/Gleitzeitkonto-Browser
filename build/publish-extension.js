b0b2ec99234ba0b4d58aceb381ede85db359e925
NilsPvR <nilspvr@pm.me>
2024-07-31 13:20:11 +0200

    refactor: sort scripts in separate folders
    updated build scripts accordingly

--- build/publish-extension.js
+++ build/publish-extension.js
@@ -8,7 +8,7 @@ const { extname } = require('path');
             return extname(src) != '.js' && extname(src) != '.scss' && extname(src) != '.env'; // exclude js and scss files since these will be created by other npm scripts
         };
         await copy('./src/extension', './compressed/extension', { filter: filterFunc }); // copies directory with subdirectories
-        fs.rmSync('./compressed/extension/url.json', { force: true }); // remove url file
+        fs.rmSync('./compressed/extension/contentscript/url.json', { force: true }); // remove url file
         fs.rmSync('./compressed/extension/.env', { force: true }); // remove secrets
         if (!fs.existsSync('./compressed/gleitzeitkonto-browser-zip')) fs.mkdirSync('./compressed/gleitzeitkonto-browser-zip'); // create folder
         
