5bdcc64ca740428ca1c4e2fa6c9b38da625a5694
NilsPvR <nilspvr@pm.me>
2024-08-09 12:19:19 +0200

    feat: convert background to ts, remvoe unused code

--- src/extension/backgroundscript/backgroundscript.ts
+++ src/extension/backgroundscript/backgroundscript.ts
@@ -1,35 +1,18 @@
-const browser = require('webextension-polyfill');
+import browser from 'webextension-polyfill';
+import { BackgroundCommand } from '../common/enums/command';
 
 // Constants
-const applicationName = 'gleitzeitkonto_browser_companionapp';
 const errorMsgs = {
-    errorInCompanionApp: '(intern) Fehler in der CompanionApp.',
-    companionAppNotFound: 'Keine Verbindung zur CompanionApp. Bitte erneut installieren.',
     invalidRequest: 'Anfrage von ungültiger extension ID erhalten. Anfrage wird abgelehnt.',
+    invalidCommand: 'Interner Fehler: ungültiger Befehl!',
 };
 
-let portFromCS; // port form content script
+let portFromCS: browser.Runtime.Port; // port form content script
 
-// returns a promise which will resole to the received message or an error message
-// message should be a object which can be sent as JSON
-async function sendMessageToCompanionApp(message) {
-    try {
-        return await browser.runtime.sendNativeMessage(applicationName, message);
-    } catch (err) {
-        console.error(err);
-
-        const command = message?.command.toLowerCase();
-        portFromCS.postMessage({
-            command: command,
-            error: { message: errorMsgs.companionAppNotFound },
-        });
-    }
-}
-
-function connectedToContentScript(port) {
+function connectedToContentScript(port: browser.Runtime.Port) {
     portFromCS = port;
 
-    if (portFromCS.sender.id !== browser.runtime.id) {
+    if (portFromCS.sender?.id !== browser.runtime.id) {
         // sender id is not the one of this extension
         // invalid id, incoming request might be malicious
         console.error(errorMsgs.invalidRequest);
@@ -38,26 +21,18 @@ function connectedToContentScript(port) {
 
     // will only receive messages meant to be sent to companion app
     portFromCS.onMessage.addListener((message) => {
-        sendMessageToCompanionApp(message).then((response) => {
-            // send command as is to companion app
-            // check for any erros from the companionApp
-            if (response?.error?.message) {
-                console.error(
-                    'Error in the Gleitzeitkonto-Browser CompanionApp: ' + response.error.message,
-                );
+        switch (message?.command) {
+            case BackgroundCommand.calculateOvertime:
+                // TODO actually calculate the overtime from the received data
+                portFromCS.postMessage({ accountString: '100h' });
+                break;
 
-                const command = message?.command.toLowerCase();
-                portFromCS.postMessage({
-                    command: command,
-                    error: { message: errorMsgs.errorInCompanionApp },
-                });
-            } else if (response) {
-                // there is actually a response
-                portFromCS.postMessage(response); // send the response as is back to the content script
-            }
-        });
+            default:
+                portFromCS.postMessage({ error: { message: errorMsgs.invalidCommand } });
+                break;
+        }
     });
 }
 
-// Listen for connection opening from the content script
+// listen for connection opening from the content script
 browser.runtime.onConnect.addListener(connectedToContentScript);
