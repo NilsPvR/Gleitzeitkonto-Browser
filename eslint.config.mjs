25b6bdf5c87a164ef84043cf92e7a1ccaa9d2dbe
NilsPvR <nilspvr@pm.me>
2024-08-12 11:11:30 +0200

    build: update eslint and add ts config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,25 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
     {
-        files: ['**/*.js'],
+        files: ['**/*.js', '**/*.ts'],
         languageOptions: { sourceType: 'module' },
     },
     {
-        files: ['src/**/*.js'],
+        files: ['src/**/*.js', 'src/**/*.ts'],
         languageOptions: { globals: globals.browser },
     },
     {
-        files: ['build/**/*.js'],
+        files: ['build/**/*.js', 'build/**/*.ts'],
         languageOptions: { globals: globals.node },
     },
     {
         ignores: ['compressed/'],
     },
     pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
     eslintConfigPrettier,
 ];
