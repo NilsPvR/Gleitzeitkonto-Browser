958911f272a8da2fc6d493bb568b707f59f91c5c
NilsPvR <nilspvr@pm.me>
2024-09-25 12:10:48 +0200

    style + docs: spacing and line breaks + capitalize

--- src/extension/contentscript/model/statusedPromise.ts
+++ src/extension/contentscript/model/statusedPromise.ts
@@ -2,14 +2,16 @@
  * Promise wrapper which adds a simple status.
  */
 export default class StatusedPromise<T extends Promise<unknown>> {
-    /** true once the promise resoles or throws an error */
+    /** True once the promise resoles or throws an error */
     public isResolved = false;
 
     public constructor(public promise: T) {
-        promise.then(() => {
-            this.isResolved = true;
-        }).catch(() => {
-            this.isResolved = true;
-        });
+        promise
+            .then(() => {
+                this.isResolved = true;
+            })
+            .catch(() => {
+                this.isResolved = true;
+            });
     }
 }
